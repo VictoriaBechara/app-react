2072eef74a2ebc98b8e2516c439a646958fa39b5
Victória Bechara <victoria.bechara@unesp.br>
2022-12-10 14:53:10 -0300

    Create form component

--- src/components/pages/project/ProjectForm.js
+++ src/components/pages/project/ProjectForm.js
@@ -1,5 +1,27 @@
-function ProjectForm() {
-  return <p>formsss</p>;
+import Input from "../../form/Input";
+import Select from "../../form/Select";
+import SubmitButton from "../../form/SubmitButton";
+import styles from "./ProjectForm.module.css";
+
+function ProjectForm({ btnText }) {
+  return (
+    <form className={styles.form}>
+      <Input
+        type="text"
+        text="Nome do Projeto"
+        name="name"
+        placeholder="Insira o nome do projeto"
+      />
+      <Input
+        type="number"
+        text="Orçamento"
+        name="budget"
+        placeholder="Insira o orçamento aqui"
+      />
+      <Select name="category_id" text="Selecione a categoria" />
+      <SubmitButton text={btnText} />
+    </form>
+  );
 }
 
 export default ProjectForm;

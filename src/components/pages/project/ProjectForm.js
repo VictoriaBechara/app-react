e491b1a8362e98e1d867e2045a1911bed1f1fb17
Victória Bechara <victoria.bechara@unesp.br>
2022-12-10 15:41:29 -0300

    add database

--- src/components/pages/project/ProjectForm.js
+++ src/components/pages/project/ProjectForm.js
@@ -3,7 +3,25 @@ import Select from "../../form/Select";
 import SubmitButton from "../../form/SubmitButton";
 import styles from "./ProjectForm.module.css";
 
+import { useEffect, useState } from "react";
+
 function ProjectForm({ btnText }) {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCategories(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <form className={styles.form}>
       <Input
@@ -18,7 +36,11 @@ function ProjectForm({ btnText }) {
         name="budget"
         placeholder="Insira o orçamento aqui"
       />
-      <Select name="category_id" text="Selecione a categoria" />
+      <Select
+        name="category_id"
+        text="Selecione a categoria"
+        options={categories}
+      />
       <SubmitButton text={btnText} />
     </form>
   );
